d00c213abbbb8ff3eb82a963c0e8596847251535
mistruniversal <152072458+mistruniversal@users.noreply.github.com>
2024-07-31 21:11:48 +0400

    Add files via upload

--- pattern.py
+++ pattern.py
@@ -426,90 +426,164 @@ import time
 
 
 
-from abc import ABC, abstractmethod
+# from abc import ABC, abstractmethod
+#
+#
+# class Subject(ABC):
+#     """
+#     Интерфейс Субъекта объявляет общие операции как для Реального Субъекта, так
+#     и для Заместителя. Пока клиент работает с Реальным Субъектом, используя этот
+#     интерфейс, вы сможете передать ему заместителя вместо реального субъекта.
+#     """
+#
+#     @abstractmethod
+#     def request(self) -> None:
+#         pass
+#
+#
+# class RealSubject(Subject):
+#     """
+#     Реальный Субъект содержит некоторую базовую бизнес-логику. Как правило,
+#     Реальные Субъекты способны выполнять некоторую полезную работу, которая к
+#     тому же может быть очень медленной или точной – например, коррекция входных
+#     данных. Заместитель может решить эти задачи без каких-либо изменений в коде
+#     Реального Субъекта.
+#     """
+#
+#     def request(self) -> None:
+#         print("RealSubject: Handling request.")
+#
+#
+# class Proxy(Subject):
+#     """
+#     Интерфейс Заместителя идентичен интерфейсу Реального Субъекта.
+#     """
+#
+#     def __init__(self, real_subject: RealSubject) -> None:
+#         self.__real_subject = real_subject
+#
+#     def request(self) -> None:
+#         """
+#         Наиболее распространёнными областями применения паттерна Заместитель
+#         являются ленивая загрузка, кэширование, контроль доступа, ведение
+#         журнала и т.д. Заместитель может выполнить одну из этих задач, а затем,
+#         в зависимости от результата, передать выполнение одноимённому методу в
+#         связанном объекте класса Реального Субъекта.
+#         """
+#
+#         if self.check_access():
+#             self.__real_subject.request()
+#             self.log_access()
+#
+#     def check_access(self) -> bool:
+#         print("Proxy: Checking access prior to firing a real request.")
+#         return True
+#
+#     def log_access(self) -> None:
+#         print("Proxy: Logging the time of request.", end="")
+#
+#
+# def client_code(subject: Subject) -> None:
+#     """
+#     Клиентский код должен работать со всеми объектами (как с реальными, так и
+#     заместителями) через интерфейс Субъекта, чтобы поддерживать как реальные
+#     субъекты, так и заместителей. В реальной жизни, однако, клиенты в основном
+#     работают с реальными субъектами напрямую. В этом случае, для более простой
+#     реализации паттерна, можно расширить заместителя из класса реального
+#     субъекта.
+#     """
+#
+#     # ...
+#
+#     subject.request()
+#
+#     # ...
+#
+#
+# if __name__ == "__main__":
+#     print("Client: Executing the client code with a real subject:")
+#     real_subject = RealSubject()
+#     client_code(real_subject)
+#
+#     print("")
+#
+#     print("Client: Executing the same client code with a proxy:")
+#     proxy = Proxy(real_subject)
+#     client_code(proxy)
+#
+#
+# """https://refactoringguru.cn/design-patterns#intro-patterns"""
 
 
-class Subject(ABC):
-    """
-    Интерфейс Субъекта объявляет общие операции как для Реального Субъекта, так
-    и для Заместителя. Пока клиент работает с Реальным Субъектом, используя этот
-    интерфейс, вы сможете передать ему заместителя вместо реального субъекта.
-    """
-
-    @abstractmethod
-    def request(self) -> None:
-        pass
 
 
-class RealSubject(Subject):
-    """
-    Реальный Субъект содержит некоторую базовую бизнес-логику. Как правило,
-    Реальные Субъекты способны выполнять некоторую полезную работу, которая к
-    тому же может быть очень медленной или точной – например, коррекция входных
-    данных. Заместитель может решить эти задачи без каких-либо изменений в коде
-    Реального Субъекта.
-    """
-
-    def request(self) -> None:
-        print("RealSubject: Handling request.")
 
 
-class Proxy(Subject):
-    """
-    Интерфейс Заместителя идентичен интерфейсу Реального Субъекта.
-    """
-
-    def __init__(self, real_subject: RealSubject) -> None:
-        self.__real_subject = real_subject
-
-    def request(self) -> None:
-        """
-        Наиболее распространёнными областями применения паттерна Заместитель
-        являются ленивая загрузка, кэширование, контроль доступа, ведение
-        журнала и т.д. Заместитель может выполнить одну из этих задач, а затем,
-        в зависимости от результата, передать выполнение одноимённому методу в
-        связанном объекте класса Реального Субъекта.
-        """
-
-        if self.check_access():
-            self.__real_subject.request()
-            self.log_access()
-
-    def check_access(self) -> bool:
-        print("Proxy: Checking access prior to firing a real request.")
-        return True
-
-    def log_access(self) -> None:
-        print("Proxy: Logging the time of request.", end="")
 
 
-def client_code(subject: Subject) -> None:
-    """
-    Клиентский код должен работать со всеми объектами (как с реальными, так и
-    заместителями) через интерфейс Субъекта, чтобы поддерживать как реальные
-    субъекты, так и заместителей. В реальной жизни, однако, клиенты в основном
-    работают с реальными субъектами напрямую. В этом случае, для более простой
-    реализации паттерна, можно расширить заместителя из класса реального
-    субъекта.
-    """
-
-    # ...
-
-    subject.request()
-
-    # ...
 
 
-if __name__ == "__main__":
-    print("Client: Executing the client code with a real subject:")
-    real_subject = RealSubject()
-    client_code(real_subject)
 
-    print("")
 
-    print("Client: Executing the same client code with a proxy:")
-    proxy = Proxy(real_subject)
-    client_code(proxy)
+# class CplusDModel:
+#     """
+#     Класс CplusDModel представляет собой реализацию модели данных.
+#     В модели хранятся значения переменных c, d и их сумма.
+#     Модель предоставляет интерфейс, через который можно работать
+#     с хранимыми значениями.
+#
+#     Модель содержит методы регистрации, удаления и оповещения
+#     наблюдателей.
+#     """
+#     def __init__( self ):
+#         self._mC = 0
+#         self._mD = 0
+#         self._mSum = 0
+#
+#         self._mObservers = [] # список наблюдателей
+#
+#     @property
+#     def c( self ):
+#         return self._mC
+#
+#     @property
+#     def d( self ):
+#         return self._mD
+#
+#     @property
+#     def sum( self ):
+#         return self._mSum
+#
+#     @c.setter
+#     def c( self, value ):
+#         self._mC = value
+#         self._mSum = self._mC + self._mD
+#         self.notifyObservers()
+#
+#     @d.setter
+#     def d( self, value ):
+#         self._mD = value
+#         self._mSum = self._mC + self._mD
+#         self.notifyObservers()
+#
+#     def addObserver( self, inObserver ):
+#         self._mObservers.append( inObserver )
+#
+#     def removeObserver( self, inObserver ):
+#         self._mObservers.remove( inObserver )
+#
+#     def notifyObservers( self ):
+#         for x in self._mObservers:
+#             x.modelIsChanged()
 
-    
-"""https://refactoringguru.cn/design-patterns#intro-patterns"""
+
+
+
+
+
+
+"""python -m venv venv-создает виртуальное окружение
+venv\Scripts\ activate-Активируем виртуальное окружение
+в коневой папке Course_First_progect активируем manage.py командой=python manage.py runserver
+
+"""
